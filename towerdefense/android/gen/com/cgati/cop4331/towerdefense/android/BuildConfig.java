c85d7440ac53eebb82007c317bce3f122eb44a3d
Paul Vitale <dominis.magic@gmail.com>
2013-02-07 02:11:44 -0500

    removed towerdefense folder

--- towerdefense/android/gen/com/cgati/cop4331/towerdefense/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.cgati.cop4331.towerdefense.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
